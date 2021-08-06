f50208cf5e9848e189869c6d1e136abdbea0bba0
ninhdochi <76436328+ninhdochi@users.noreply.github.com>
2021-08-06 14:23:26 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
 provider "aws" {
   region = "us-west-1"
-  version = "~> 3.27"
 }
 
 resource "aws_default_vpc" "default" {}
